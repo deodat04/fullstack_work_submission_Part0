17ee329f08200a93e889ad5e2332dec16ac4ab4a
Déodat ADANDEDJAN <deodatadandedjan2000@gmail.com>
2024-02-19 01:08:23 +0100

    step3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log(props);
   return (
     <div>
       <h1>{props.course}</h1>
@@ -24,7 +25,7 @@ const Part = (props) => {
       </p>
     </div>
   );
-}
+};
 
 const Total = (props) => {
   return (
@@ -32,32 +33,53 @@ const Total = (props) => {
       <p> Number of exercises is {props.total} </p>
     </div>
   );
-
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const part2 = "Using props to pass data";
-  const part3 = "State of a component";
-  const exercises1 = 10;
-  const exercises2 = 7;
-  const exercises3 = 14;
-
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
+
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
 
 export default App;
+
+// const t = [1, -1, 3]
+
+// const t2 = t.concat(5)
+
+// console.log(t)  // [1, -1, 3] est affiché
+// console.log(t2) // [1, -1, 3, 5] est affiché
+
+// const t = [1, 2, 3]
+
+// const m1 = t.map(value => value * 2)
+// console.log(m1)   // [2, 4, 6] est affiché
