d2bff40b13ce8cc640f68cbe39b560ebebcdf087
Déodat ADANDEDJAN <deodatadandedjan2000@gmail.com>
2024-02-20 13:33:46 +0100

    update counter using {useState}

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+const App = () => {
+
+  const [ counter, setCounter ] = useState(0)
+
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+  console.log('rendering...', counter)
+
   return (
     <div>{counter}</div>
   )
 }
 
 export default App
-
-
-
-
-
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import App from "./App";
+import App from './App'
 
-let counter = 1
-
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-  )
-}
-
-setInterval(() => {
-    refresh()
-    counter += 1
-  }, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
