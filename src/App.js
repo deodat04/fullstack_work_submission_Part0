22ca18d684eeecab5e53421439dbc86f79fdc16f
Déodat ADANDEDJAN <deodatadandedjan2000@gmail.com>
2024-02-20 14:19:21 +0100

    handleClick

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
 import { useState } from "react";
 
-
-
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) => {
-  return (
-    <button onClick = {onClick}>
-      {text}
-    </button>
-  )
-}
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
 
-  const increaseByOne = () => setCounter(counter + 1);
+  const handleLeftClick = () => {
+    const newClicks = { 
+      left: clicks.left + 1, 
+      right: clicks.right 
+    }
+    setClicks(newClicks)
+  }
 
-  const decreaseByOne = () => setCounter(counter - 1);
-
-  const setToZero = () => setCounter(0);
+  const handleRightClick = () => {
+    const newClicks = { 
+      left: clicks.left, 
+      right: clicks.right + 1 
+    }
+    setClicks(newClicks)
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />           
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
-  );
+  )
 };
 
 export default App;
