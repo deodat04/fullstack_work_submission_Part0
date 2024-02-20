cc6c49e84f0b5cc5dafac161efcbd9731a591946
Déodat ADANDEDJAN <deodatadandedjan2000@gmail.com>
2024-02-20 13:54:26 +0100

    button for counter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
 
-  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1);
 
+  const decreaseByOne = () => setCounter(counter - 1);
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  console.log('rendering...', counter)
+  const setToZero = () => setCounter(0);
 
   return (
-    <div>{counter}</div>
-  )
-}
+    <div>
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
+    </div>
+  );
+};
 
-export default App
-
-
-
-
-
+export default App;
 
 // const Header = (props) => {
 //   console.log(props);
