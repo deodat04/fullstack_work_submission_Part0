938b2f11ecf1331a93e18fcc0a75aad0c7f60449
Déodat ADANDEDJAN <deodatadandedjan2000@gmail.com>
2024-02-20 13:15:23 +0100

    composant | déstructuration

--- src/App.js
+++ src/App.js
@@ -7,12 +7,33 @@ const Header = (props) => {
   );
 };
 
+const Hello = ({ name, age }) => {
+  //const { name, age } = props
+  const bornYear = () => new Date().getFullYear() - age;
+
+  return (
+    <div>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  );
+};
+
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
-      <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
-      <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
+      <Part
+        part={props.course.parts[0].name}
+        exercises={props.course.parts[0].exercises}
+      />
+      <Part
+        part={props.course.parts[1].name}
+        exercises={props.course.parts[1].exercises}
+      />
+      <Part
+        part={props.course.parts[2].name}
+        exercises={props.course.parts[2].exercises}
+      />
     </div>
   );
 };
@@ -30,34 +51,46 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p> Number of exercises is {props.course.parts[0].exercises+props.course.parts[1].exercises+props.course.parts[2].exercises} </p>
+      <p>
+        {" "}
+        Number of exercises is{" "}
+        {props.course.parts[0].exercises +
+          props.course.parts[1].exercises +
+          props.course.parts[2].exercises}{" "}
+      </p>
     </div>
   );
 };
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  const name = "Santos";
+  const age = 20;
 
   return (
     <div>
       <Header course={course.name} />
-
+      <h2>Greetings</h2>
+      <Hello name="Jean" age={20 + 2} />
+      <Hello name={name} age={age} />
+      <h2>Contents</h2>
       <Content course={course} />
 
       <Total course={course} />
@@ -78,3 +111,21 @@ export default App;
 
 // const m1 = t.map(value => value * 2)
 // console.log(m1)   // [2, 4, 6] est affiché
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greet() {
+    console.log(
+      "hello, my name is " + this.name + "and i have " + this.age + " years old"
+    );
+  }
+}
+
+const adam = new Person("Adam Ondra", 35);
+adam.greet();
+
+const janja = new Person("Janja Garnbret", 22);
+janja.greet();
